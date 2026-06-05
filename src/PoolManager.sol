b38f96932726ca64014d9e91d35c62f58afd9c4e
Edmundo Arias <Edmpulasky@gmail.com>
2026-06-05 00:30:14 -0500

    handle initialize and modify liquidity flash accounting

--- src/PoolManager.sol
+++ src/PoolManager.sol
@@ -49,4 +49,100 @@ abstract contract PoolManager is IPoolManager, ProtocolFees, NoDelegateCall, ERC
 
         _;
     }
+
+    function unlock(bytes memory data) external override returns (bytes memory result) {
+        if (Lock.isUnlocked()) AlreadyUnlocked.selector.revertWith();
+
+        Lock.unlock();
+
+        result = IUnlockCallback(msg.sender).unlockCallback(data);
+
+        if (NonzeroDeltaCount.read() != 0) CurrencyNotSettled.selector.revertWith();
+
+        Lock.lock();
+    }
+
+    function initialize(PoolKey memory key, uint160 sqrtPriceX96) external noDelegateCall returns (int24 tick) {
+        if (key.tickSpacing > MAX_TICK_SPACING) TickSpacingTooLarge.selector.revertWith(key.tickSpacing);
+        if (key.tickSpacing < MIN_TICK_SPACING) TickSpacingTooSmall.selector.revertWith(key.tickSpacing);
+
+        if (key.currency0 >= key.currency1) {
+            CurrenciesOutOfOrderOrEqual.selector
+                .revertWith(Currency.unwrap(key.currency0), Currency.unwrap(key.currency1));
+        }
+
+        if (!key.hooks.isValidHookAddress(key.fee)) Hooks.HookAddressNotValid.selector.revertWith(address(key.hooks));
+
+        uint24 lpFee = key.fee.getInitialLPFee();
+
+        key.hooks.beforeInitialize(key, sqrtPriceX96);
+
+        PoolId id = key.toId();
+        tick = _pools[id].initialize(sqrtPriceX96, lpFee);
+
+        emit Initialize(id, key.currency0, key.currency1, key.fee, key.tickSpacing, key.hooks, sqrtPriceX96, tick);
+
+        key.hooks.afterInitialize(key, sqrtPriceX96, tick);
+    }
+
+    function modifyLiquidity(PoolKey memory key, ModifyLiquidityParams memory params, bytes calldata hookData)
+        external
+        override
+        onlyWhenUnlocked
+        noDelegateCall
+        returns (BalanceDelta callerDelta, BalanceDelta feesAccrued)
+    {
+        PoolId id = key.toId();
+        {
+            Pool.State storage pool = _getPool(id);
+            pool.checkPoolInitialized();
+
+            key.hooks.beforeModifyLiquidity(key, params, hookData);
+
+            BalanceDelta principleDelta;
+            (principleDelta, feesAccrued) = pool.modifyLiquidity(
+                Pool.ModifyLiquidityParams({
+                    owner: msg.sender,
+                    tickLower: params.tickLower,
+                    tickUpper: params.tickUpper,
+                    liquidityDelta: params.liquidityDelta.toInt128(),
+                    tickSpacing: key.tickSpacing,
+                    salt: params.salt
+                })
+            );
+
+            callerDelta = principleDelta + feesAccrued;
+        }
+
+        emit ModifyLiquidity(id, msg.sender, params.tickLower, params.tickUpper, params.liquidityDelta, params.salt);
+
+        BalanceDelta hookDelta;
+        (callerDelta, hookDelta) = key.hooks.afterModifyLiquidity(key, params, callerDelta, feesAccrued, hookData);
+
+        if (hookDelta != BalanceDeltaLibrary.ZERO_DELTA) _accountPoolBalanceDelta(key, hookDelta, address(key.hooks));
+
+        _accountPoolBalanceDelta(key, callerDelta, msg.sender);
+    }
+
+    function _accountDelta(Currency currency, int128 delta, address target) internal {
+        if (delta == 0) return;
+
+        (int256 previous, int256 next) = currency.applyDelta(target, delta);
+
+        if (next == 0) {
+            NonzeroDeltaCount.decrement();
+        } else if (previous == 0) {
+            NonzeroDeltaCount.increment();
+        }
+    }
+
+    function _accountPoolBalanceDelta(PoolKey memory key, BalanceDelta delta, address target) internal {
+        _accountDelta(key.currency0, delta.amount0(), target);
+        _accountDelta(key.currency1, delta.amount1(), target);
+    }
+
+    /// @notice Implementation of the _getPool function defined in ProtocolFees
+    function _getPool(PoolId id) internal view override returns (Pool.State storage) {
+        return _pools[id];
+    }
 }
